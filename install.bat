2904f5434a49b375c0be4fd8541606bed2bd50ed
Luci_führer <36377313+AlapanGhosh@users.noreply.github.com>
2018-09-22 14:57:37 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Installing Python
-start python-3.6.5-amd64.exe
+start python-3.7.0-amd64-webinstall.exe
 set /p Var1="Press Enter when Python Installation is complete"
 cls
 pip install easyimap
